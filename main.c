e09258330ef2ab4bf71f5214ff46aeaf42587b35
Gunnar Þór Magnússon <gunnar.magnusson@booking.com>
2017-10-26 07:30:09 +0200

    Print error strings

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <ctype.h>
+#include <errno.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void printplan(FILE* fh);
+char* plan_name(void);
+
+int main(int argc, char* argv[])
+{
+    char* filename = plan_name();
+    FILE* fh = fopen(filename, "r");
+    if (fh == NULL) {
+        fprintf(stderr, "%s\n", strerror(errno));
+    }
+    printplan(fh);
+    fclose(fh);
+
+    return EXIT_SUCCESS;
+}
 
 enum state {
     SKIP_ALL,
@@ -45,22 +63,18 @@ void printplan(FILE* fh)
     }
 }
 
-int main(int argc, char* argv[])
-{
+char* plan_name(void) {
     char* home = getenv("HOME");
     if (home == NULL) {
         fprintf(stderr, "Environment variable HOME is not defined\n");
-        return EXIT_FAILURE;
+        return NULL;
     }
 
     char* filename;
     if (asprintf(&filename, "%s/.plan", home) == -1) {
         fprintf(stderr, "Unable to construct path to .plan file\n");
-        return EXIT_FAILURE;
+        return NULL;
     }
 
-    FILE* fh = fopen(filename, "r");
-    printplan(fh);
-
-    return EXIT_SUCCESS;
+    return filename;
 }
